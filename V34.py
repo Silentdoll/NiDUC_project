c5ffeca677d45288d81c9774ddbce2b9811ab718
Silentdoll <michal1110@o2.pl>
2018-04-09 02:05:10 +0200

    Scrambler V34

---
+++ V34.py
@@ -0,0 +1,38 @@
+﻿# Scramlber V.34
+
+import random
+from operator import xor
+
+data = [1,1,0,1,0,1,0,0,1,1,0,0,1,1,0,1]
+out = []
+
+x  = 7
+
+SYNC = []
+for sync_i in range(0,x):
+    SYNC.append(random.randint(0,1))
+
+print("SYNC: ", SYNC)
+print("DATA:   ", data)
+
+data_len = int(len(data))
+print("Data count: ", data_len)
+
+for i in range(0,data_len):
+
+    XOR_VALUE = xor(SYNC[x-1],SYNC[x-2])
+
+    SYNC.insert(0,SYNC.pop())
+    SYNC[0]=XOR_VALUE
+
+    out.append(xor(XOR_VAL,data[i]))
+
+print("Data output: ", out)
+
+
+
+
+
+
+
+
